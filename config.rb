509afeae9604fddb4e743da83d6eb9638d4a18cd
nulltask <nulltask@gmail.com>
2013-12-13 20:23:19 +0900

    update config

--- config.rb
+++ config.rb
@@ -54,19 +54,23 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+after_configuration do
+  sprockets.append_path "#{root}/bower_components/"
 end
 
 activate :deploy do |deploy|
