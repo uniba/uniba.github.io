4ad1dbc9e997ffec3b5c8426ca76269ed63d9148
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2014-03-31 20:25:21 +0900

    jsをminifyしないように戻しつつコメントを併記

--- config.rb
+++ config.rb
@@ -63,8 +63,9 @@ configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
 
+  # Minifyするとエラーする
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
