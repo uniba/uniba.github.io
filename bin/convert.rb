2d7bd51a0df92c00a168ae4a223f809987fb1faf
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2018-09-18 19:28:11 +0900

    csvtojsonの入れ子をべた書き

--- bin/convert.rb
+++ bin/convert.rb
@@ -10,25 +10,30 @@ class Converter
 
   def exec
     Dir.glob("#{@data_dir}/*.csv") do |path|
-      csv = CSV.open(path, return_headers: false, headers: :first_row)
+      csv = CSV.open(path, return_headers: false, headers: true)
       basename = File.basename(path, '.csv')
       method = "convert_#{basename}".to_sym
       result = (self.send(method, csv) rescue puts "WARNING: #{method} is undefined.")
       json_path = "#{@data_dir}/#{basename}.json"
       File.write(json_path, JSON.pretty_generate(result))
+      puts "convert done."
     end
   end
 
   def convert_events(csv)
     csv.to_a.map do |row|
       {
-        publish: row[0],
+        published: row[0],
         date: row[1],
         thumb_url: row[2],
         primary_url: row[3],
         title: row[4],
         description: row[5],
-        # urls: row[6..9].select { |url| url =~ /^https?:\/\// }
+        documents: [
+          { title: row[6], url: row[7] },
+          { title: row[8], url: row[9] },
+          { title: row[10], url: row[11] }
+        ]
       }
     end
   end
