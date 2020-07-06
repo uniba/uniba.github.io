02e55cb3a3f1288e1af8510dcb356bf3b2959eea
Satoshi Motizuki <satoshi@uniba.jp>
2020-07-06 18:03:57 +0900

    google からのダウンロードがうまくいってなかったので修正

--- bin/download.sh
+++ bin/download.sh
@@ -7,15 +7,16 @@ echo " Downloading CSV from Google spreadsheets..."
 echo
 
 manuscripts=(
-  "1Um0wM3RrUaByQyzcusi9u7vi8YrfWzE1Qty0txavSrY 0 data/events.csv"
+  "1Um0wM3RrUaByQyzcusi9u7vi8YrfWzE1Qty0txavSrY data/events.csv"
 )
 
 for ((i = 0; i < ${#manuscripts[@]}; i++)) {
   set -- ${manuscripts[i]}
-  echo " [$(($i + 1))/${#manuscripts[@]}] Downloading: $3"
-  curl --silent "https://docs.google.com/spreadsheets/d/$1/export?gid=$2&format=csv&id=$2" > $3
+  echo " [$(($i + 1))/${#manuscripts[@]}] Downloading: $2"
+  curl --silent -L "https://docs.google.com/spreadsheets/d/$1/export?format=csv" > $2
 }
 
 echo
+echo " https://docs.google.com/spreadsheets/d/$1/export?format=csv"
 echo " All done."
 echo
